b90c3e6a2436d484b87e3483fe06e0d8c0f7f5d1
Ivan Leichtling <ivanlei@yelp.com>
2015-04-24 22:59:20 -0700

    Parse only known args to allow maltego

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='threatbutt',
-    version='1.0.1',
+    version='1.0.2',
     provides=['threatbutt'],
     author='c0wl',
     url='https://github.com/ivanlei/threatbutt',
